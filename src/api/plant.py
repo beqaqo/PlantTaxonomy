d04b08e1012bca2fd8125c1ec6c3621ae8720c72
ბექა <beka.kopadze.2@iliauni.edu.ge>
2025-07-03 18:27:58 +0400

    added search with other plant model attributes

--- src/api/plant.py
+++ src/api/plant.py
@@ -1,8 +1,8 @@
 from flask_restx import Resource, reqparse
+from sqlalchemy import or_
 
 from src.ext import api
 from src.models import Plant
-
 
 parser = reqparse.RequestParser()
 parser.add_argument("page", type=int, default=1, required=True)
@@ -11,10 +11,11 @@ parser.add_argument("sort", type=str, required=False)
 
 limit = 20
 
+
 @api.route("/plant")
 class PlantAPI(Resource):
     @api.expect(parser)
-    def get(self,):
+    def get(self, ):
         args = parser.parse_args()
         page = args['page']
         search = args['search']
@@ -23,7 +24,14 @@ class PlantAPI(Resource):
         plants = Plant.query
 
         if search:
-            plants = plants.filter(Plant.name.ilike(f"%{search}%"))
+            plants = plants.filter(
+                or_(
+                    Plant.name.ilike(f"%{search}%"),
+                    Plant.eng_name.ilike(f"%{search}%"),
+                    Plant.family_name.ilike(f"%{search}%"),
+                    Plant.family_name_geo.ilike(f"%{search}%")
+                )
+            )
 
         if sort == "asc":
             plants = plants.order_by(Plant.name.asc())
@@ -35,8 +43,8 @@ class PlantAPI(Resource):
         plants = plants.limit(limit).offset((page - 1) * limit)
 
         plants_json = [{"id": plant.id,
-                        "name" : plant.name,
-                        "eng_name" : plant.eng_name,
+                        "name": plant.name,
+                        "eng_name": plant.eng_name,
                         "family_name": plant.family_name,
                         "family_name_geo": plant.family_name_geo,
                         "description": plant.description,
@@ -54,13 +62,14 @@ class PlantAPI(Resource):
 
         return response, 200
 
+
 @api.route("/plant/<int:id>")  # Route for a specific plant
 class PlantAPI(Resource):
     def get(self, id):
         plant = Plant.query.get_or_404(id)
         plants_json = [{"id": plant.id,
-                        "name" : plant.name,
-                        "eng_name" : plant.eng_name,
+                        "name": plant.name,
+                        "eng_name": plant.eng_name,
                         "family_name": plant.family_name,
                         "family_name_geo": plant.family_name_geo,
                         "description": plant.description,
